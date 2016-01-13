69bd931327577fd2598739843334a199e740dde0
Varghese George <vg@vargheseg.me>
2016-01-13 18:35:04 +0530

    Fix connect.inc.php
    Fix Typos and DB name in connect.inc.php file

--- connect.inc.php
+++ connect.inc.php
@@ -2,11 +2,11 @@
 	$dbhost = "localhost";
 	$dbuser = "jobFair";
 	$dbpass = "cultJobFair2016";
-	$dbname = "jobFairDB";
+	$dbname = "jobFair_DB";
 
 	$conn_error = "Could Not Connect!";
 
-	if(!@mysql_connect($dbhost, $dbuser, $dbpass) || !@mysql_select_db($dbname))
+	if(!mysql_connect($dbhost, $dbuser, $dbpass) || !mysql_select_db($dbname))
 	{
 		die($conn_error);
 	}
